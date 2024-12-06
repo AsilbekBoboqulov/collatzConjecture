01de219f83dcab45894c1d5bda915e2e53622eab
AsilbekBoboqulov <93362474+AsilbekBoboqulov@users.noreply.github.com>
2024-12-07 04:37:13 +0500

    Create becomeSmaller.cpp

---
+++ becomeSmaller.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define sc second
+#define int int64_t
+pair<int, int> f(int i, int j){
+    if(j < 2 || i < 2) return {i, j};
+    else if(i % 2 < 1 && j % 2 < 1) return f(i / 2, j / 2);
+    else if(i % 2 < 1 && j % 2) return f(3 * i, 3 * j + 1);
+    else return {i, j};
+}
+signed main(){
+    for(int i = 2; i < 100; i++){
+        for(int j = 1; j < i; j += 2){
+            pair<int, int> pr = f(i, j);
+            if(pr.ff < i && pr.sc < j) cout << i << " X + " << j << "    to    " << pr.ff << " X + " << pr.sc << "\n";
+        }
+    }
+}
